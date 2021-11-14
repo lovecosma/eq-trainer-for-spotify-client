7705d2ee4e43fa74fd1136bf11b7d373187debad
love.cosma <williamsmatthew333@gmail.com>
2021-11-13 18:53:01 -0700

    Implementing play and stop functionality along with ux

--- src/components/Spectrum.jsx
+++ src/components/Spectrum.jsx
@@ -1,20 +1,12 @@
 import React from 'react'
 import Sketch from "react-p5"
 import * as Tone from "tone"
-export default function Spectrum({track}) {
+export default function Spectrum({playing, fft}) {
 
     let button;
-    let fft
     let spectrum
     let biquad; let player
-
-    const preload = () => {
-        biquad = new Tone.BiquadFilter(200, 'lowpass')
-        player =  new Tone.Player(track.preview_url, () => {
-            player.connect(biquad).toDestination()
-        })
-    }
-  
+ 
     const toggleSong = () => {
 
     }
@@ -22,22 +14,21 @@ export default function Spectrum({track}) {
         p5.createCanvas(canvasParentRef.clientWidth * 0.90, 250).parent(canvasParentRef);
         // p5.colorMode("HSB");
         // p5.angleMode("DEGREES");
-
-        // fft = new Tone.FFT(32).toDestination()
     }
 
     const draw = (p5, canvasParentRef) => {
         p5.background(255)
-        // spectrum = fft.getValue()
-        // console.log(spectrum)
+       if(playing){
+    //     spectrum = fft.getValue()
+    //     console.log(spectrum)
+       }
         // p5.stroke(255)
         // p5.noFill()
     }
 
-
     return (
         <div className="center">
-            <Sketch preload={preload} setup={setup} draw={draw} />
+            <Sketch setup={setup} draw={draw} />
         </div>
     )
 }
--- src/containers/PlaygroundTrackCard.jsx
+++ src/containers/PlaygroundTrackCard.jsx
@@ -1,20 +1,39 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import EQUI from '../components/EQUI'
 import "../stylesheets/PlaygroundTrackCard.css"
 import TrackInfo from "../components/TrackInfo"
 import PlayerUI from '../components/PlayerUI'
 import TrackArt from '../components/TrackArt'
 import Spectrum from '../components/Spectrum'
+import * as Tone from "tone"
+
 export default function PlaygroundTrackCard({track}) {
 
     const [playing, setPlaying] = useState(false)
+    const [player, setPlayer] = useState(new Tone.Player(track.preview_url, () => {
+        player.connect(biquad).connect(fft).toDestination()
+    }))
+    const [fft, setFft] = useState(new Tone.FFT(32));
+    const [biquad, setBiquad] = useState(new Tone.BiquadFilter(200, 'lowpass'))
+
+
+    useEffect(() => {
+        player.stop()
+        setPlaying(false)
+        setPlayer( new Tone.Player(track.preview_url, () => {
+        player.connect(biquad).connect(fft).toDestination()
+    }))
+    }, [track])
 
     const play = async () => {
         setPlaying(true)
+        player.start()
+        
     }
 
     const stop = async () => {
         setPlaying(false)
+        player.stop()
     }
 
 
@@ -24,7 +43,7 @@ export default function PlaygroundTrackCard({track}) {
             <TrackArt track={track}/>
             <TrackInfo track={track} play={play} playing={playing} stop={stop}/>
             </div>
-            {playing ? <Spectrum track={track}/> : <h1>Not Playing</h1>}
+            <Spectrum playing={playing} fft={fft}/> 
        </div>
    )
 }
