e6c10287adcba2e0a78d9f8286397e6511d5a771
love.cosma <williamsmatthew333@gmail.com>
2021-12-06 18:48:17 -0700

    Experimenting with graphic eq. UI.

--- src/components/Spectrum.jsx
+++ src/components/Spectrum.jsx
@@ -1,17 +1,13 @@
 import React from 'react'
 import Sketch from "react-p5"
-import * as Tone from "tone"
 export default function Spectrum({playing, fft}) {
 
     let button;
     let spectrum
     let height = 250
     let width
-    const toggleSong = () => {
-        
-    }
+
     const setup = (p5, canvasParentRef) => {
-        width = canvasParentRef.clientWidth
         p5.createCanvas(700, height).parent(canvasParentRef);
         // p5.colorMode("HSB");
         // p5.angleMode("DEGREES");
@@ -22,10 +18,12 @@ export default function Spectrum({playing, fft}) {
        if(playing){
         spectrum = fft.getValue()
         p5.stroke(255)
+        
         for(let i = 0; i < spectrum.length; i++){
             let amp = spectrum[i] 
             let y = p5.map(amp, -75, -25, height, 0)
-            p5.line(i, height, i, y)
+            let w = 700 /64
+            p5.line(i*w , height, i*w, y)
         }
        }
         // p5.stroke(255)
